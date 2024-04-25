cd98519128c66ece95079e51b2e93942e686f780
Aiden <aidenjs9@icloud.com>
2024-04-25 11:15:24 -0500

    NOO DONT TEST IT

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let name = 'ExampleJSHook'
 let bundleName = name + '.Freedeck'
 // let bundleName = 'ExamplePlugin.Freedeck'
 const FDE_Settings = {
-	test: true,
+	test: false,
 	UseScripts: true,
 	BundlePrebuild: name,
 	BundleName: bundleName,
